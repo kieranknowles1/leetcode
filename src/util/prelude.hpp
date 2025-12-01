b3d68f10cdf6733871e74fb3798844728c6e5fbb
Kieran <kieranknowles11@hotmail.co.uk>
2025-12-01 20:38:11 +0000

    support more input types for tests

--- src/util/prelude.hpp
+++ src/util/prelude.hpp
@@ -18,18 +18,34 @@ void check(int caseNum, bool ok) {
   cout << "Test " << caseNum << " " << (ok ? "OK" : "Failing") << endl;
 }
 
+template <typename T> string print(const T &value);
+
+template <> string print(const string &value) { return "\"" + value + "\""; }
+
+template <> string print(const pair<string, string> &value) {
+  return "(" + print(value.first) + ", " + print(value.second) + ")";
+}
+
 template <typename TIn, typename TOut>
 bool runTests(const TestCases<TIn, TOut> &testCases,
               function<TOut(TIn)> testFunc) {
   bool passed = true;
   for (const auto &testCase : testCases) {
     const auto &[input, expected] = testCase;
-    cout << "Test: input = " << input << ", expected = " << expected << endl;
+    cout << "Test: input = " << print(input) << ", expected = " << expected
+         << endl;
     bool result = testFunc(input) == expected;
     passed &= result;
     if (!result) {
       cout << "Test failed: actual = " << testFunc(input) << endl;
     }
   }
+
+  if (passed) {
+    cout << "Tests passed" << endl;
+  } else {
+    cout << "One or more tests failed" << endl;
+  }
+
   return passed;
 }
