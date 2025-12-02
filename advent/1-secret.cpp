f41207ee46a48a88524b6ba9b800d62e7fa85c8d
Kieran <kieranknowles11@hotmail.co.uk>
2025-12-02 20:15:23 +0000

    prep for part 2

--- advent/1-secret.cpp
+++ advent/1-secret.cpp
@@ -5,19 +5,34 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
-int decodeFile(std::filesystem::path path) {
+std::vector<std::string> readlines(std::filesystem::path path) {
   std::ifstream stream(path);
   if (stream.bad()) {
     throw std::runtime_error("Unable to read file");
   }
 
+  std::vector<std::string> out;
+  for (std::string line; std::getline(stream, line);) {
+    out.push_back(line);
+  }
+  return out;
+}
+
+int parseMovement(std::string_view line) {
+  char direction = line[0];
+  int amount = std::stoi(line.data() + 1);
+  return amount * (direction == 'R' ? 1 : -1);
+}
+
+int decodeFile(std::filesystem::path path) {
+  auto lines = readlines(path);
   int position = 50;
   int zeros = 0;
-  for (std::string line; std::getline(stream, line);) {
-    char direction = line[0];
-    int amount = std::stoi(line.data() + 1);
-    position += amount * (direction == 'R' ? 1 : -1);
+  for (auto &line : lines) {
+    int amount = parseMovement(line);
+    position += amount;
 
     if (position % 100 == 0)
       zeros++;
